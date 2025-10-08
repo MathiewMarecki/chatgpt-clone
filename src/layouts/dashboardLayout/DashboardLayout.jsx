bfd4d35f70c3bc4d2859f761c419c6a6f959354a
mathiewmarecki <mathiewmarecki.gmail.com>
2025-10-08 10:41:05 +0200

    dahboard menu done

--- src/layouts/dashboardLayout/DashboardLayout.jsx
+++ src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router'
 import './dashboardLayout.css'
 import React from 'react'
+import ChatList from '../../components/chatList/ChatList'
 
 function DashboardLayout() {
   return (
     <div className='dashboardLayout'>
-        <div className='menu'>MENU</div>
+        <div className='menu'><ChatList/></div>
         <div className='content'>
             <Outlet/>
         </div>
